8e8e525a59bd1b75dbedcac80f976ae03051b014
F119650 <andre.luizfranca@claro.com.br>
2022-07-21 17:41:31 -0300

    alteração class

--- function_credentials.py
+++ function_credentials.py
@@ -1,11 +1,8 @@
-import email
-
-
 class MinhasCredenciais:
     def __init__(self, email, senha):
         self.email = email
         self.senha = senha
-credenciais = MinhasCredenciais('<EMAIL>','<SENHA>') # inclua seu email e a senha gerada pelo google account
+credenciais = MinhasCredenciais('SEU EMAIL','SUA SENHA') # inclua seu email e a senha gerada pelo google account
 #print(credenciais.email) 
 
  
